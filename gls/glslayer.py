2eeea44ce1d35f854ffe79e472860fba902274d6
Aidan Williams <aidan19@me.com>
2022-04-04 17:01:51 -0400

    Fix device error

--- gls/glslayer.py
+++ gls/glslayer.py
@@ -72,8 +72,8 @@ class GLSLayer(nn.Module):
 class Trainer():
 
     def __init__(self, model: GLSLayer):
-        self.model = model
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = model.to(self.device)
 
 
     def load_data(self, x: np.array, y: np.array, test_size=0.2, random_state=None):
