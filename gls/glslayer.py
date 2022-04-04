9265e1b7b85f0bf35134fa68493b5336f9490144
Aidan Williams <aidan19@me.com>
2022-04-04 15:12:19 -0400

    Update type of model

--- gls/glslayer.py
+++ gls/glslayer.py
@@ -58,7 +58,7 @@ class GLSLayer(nn.Module):
 
 class Trainer():
 
-    def __init__(self, model: type[GLSLayer]):
+    def __init__(self, model: GLSLayer):
         self.model = model
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
