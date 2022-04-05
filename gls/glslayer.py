40116cbde6a9123d3255d39c22329a23e9db9f66
Aidan Williams <aidan19@me.com>
2022-04-05 15:38:51 -0400

    Forgot to make changes...

--- gls/glslayer.py
+++ gls/glslayer.py
@@ -9,7 +9,7 @@ from torch.nn import functional as F
 
 class GLSLayer(nn.Module):
 
-    def __init__(self, b: torch.Tensor, q: torch.Tensor, e: torch.Tensor, classes: int):
+    def __init__(self, b: torch.Tensor, q: torch.Tensor, e: torch.Tensor, classes: int, neurons: int):
         """Create an instance of a GLSLayer.
 
         Args:
@@ -22,12 +22,13 @@ class GLSLayer(nn.Module):
         self.b = nn.Parameter(b)
         self.q = nn.Parameter(q)
         self.e = nn.Parameter(e)
-        self.M = nn.Parameter(torch.zeros((classes, b.shape[0])))
+        self.M = nn.Parameter(torch.zeros((classes, neurons)))
+        self.M.requires_grad = False
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
     def fire_neurons(self, x: torch.Tensor):
-        m = torch.ones(x.shape[0], 1, device=self.device) * self.q # m.shape is len(x) x len(q)
+        m = torch.ones(x.shape, device=self.device) * self.q # m.shape is len(x) x len(x[0])
         k = torch.ones(m.shape, device=self.device)
         
         for idx in range(len(m)):
@@ -38,7 +39,7 @@ class GLSLayer(nn.Module):
             
             while len(m_indices):
                 m[idx] = torch.where(
-                    m[idx] < self.b,
+                    m[idx] < self.b, # maybe all need to be less than self.b?
                     m[idx] / self.b,
                     (1 - m[idx]) / (1 - self.b)
                 ) # Condition, True, False
@@ -49,9 +50,9 @@ class GLSLayer(nn.Module):
                 last_indices = m_indices
                 k[idx, m_indices] += 1
 
-                # if counter % 1000 == 0:
-                #     print("Convergence of sample {}: {}/{}".format(idx + 1, len(m[0]) - len(m_indices), len(m[0])))
-                # counter += 1
+                if counter % 100 == 0:
+                    print("Convergence of sample {}: {}/{}".format(idx + 1, len(m[0]) - len(m_indices), len(m[0])))
+                counter += 1
             
             print("Sample {} Converged".format(idx + 1))
 
@@ -77,7 +78,7 @@ class GLSLayer(nn.Module):
 
     def train(self, x: torch.Tensor, y: torch.Tensor):
         k = self.fire_neurons(x)
-        occurences = torch.zeros(self.M.shape[0])
+        occurences = torch.zeros(self.M.shape[0], device=self.device)
 
         for idx in range(len(k)):
             self.M[y[idx]] += k[idx]
@@ -93,7 +94,14 @@ class Trainer():
         self.model = model.to(self.device)
 
 
+    def normalize(self, x: np.array):
+        min_x = np.min(x)
+        x_ = x - min_x
+        return x_ / (np.max(x) - min_x)
+
+
     def load_data(self, x: np.array, y: np.array, test_size=0.2, random_state=None):
+        x = self.normalize(x)
         p = np.random.permutation(len(x))
         X = x[p]
         Y = y[p]
@@ -102,8 +110,8 @@ class Trainer():
 
         self.X_train = torch.tensor(X_train).float().to(self.device)
         self.X_test = torch.tensor(X_test).float().to(self.device)
-        self.y_train = torch.tensor(y_train).float().to(self.device)
-        self.y_test = torch.tensor(y_test).float().to(self.device)
+        self.y_train = torch.tensor(y_train).to(self.device)
+        self.y_test = torch.tensor(y_test).to(self.device)
 
 
     def train(self):
