1f751b350f55f55820bb4ce7d48c84ade505f7b8
Aidan Williams <aidan19@me.com>
2022-04-08 16:02:07 -0400

    Fix sizing issues on deep networks

--- gls/models.py
+++ gls/models.py
@@ -2,15 +2,13 @@ import torch
 from torch import nn
 import torchvision.models as models
 
-from gls.glslayer import GLSLayer
-
 
 class DeepModel(nn.Module):
 
-    def __init__(self, gls: GLSLayer):
+    def __init__(self, gls_create: function):
         super(DeepModel, self).__init__()
         self.flatten = nn.Flatten()
-        self.gls = gls
+        self.gls_create = gls_create
 
 
     def forward(self, x: torch.Tensor, logits: bool=False):
@@ -29,25 +27,27 @@ class DeepModel(nn.Module):
 
 class VGG(DeepModel):
 
-    def __init__(self, gls: GLSLayer, final_size=(7, 7)):
-        super(VGG, self).__init__(gls)
+    def __init__(self, gls_create: function, final_size=(1, 1)):
+        super(VGG, self).__init__(gls_create)
         self.vgg = models.vgg16(pretrained=True)
-        self.vgg_features = self.vgg.features
+        self.vgg_features = self.vgg.features[:-1] # Remove last pooling layer to fix sizing on mnist
         self.vgg_avg = nn.AdaptiveAvgPool2d(output_size=final_size)
         self.base = nn.Sequential(self.vgg_features, self.vgg_avg)
+        self.gls = self.gls_create(512)
 
 
 class ResNet(DeepModel):
 
-    def __init__(self, gls: GLSLayer):
-        super(ResNet, self).__init__(gls)
+    def __init__(self, gls_create: function):
+        super(ResNet, self).__init__(gls_create)
         self.resnet = models.resnet50(pretrained=True)
         self.base = nn.Sequential(*list(self.resnet.children())[:-1]) # remove last layer
-
+        self.gls = self.gls_create(2048)
 
 class EfficientNet(DeepModel):
 
-    def __init__(self, gls: GLSLayer):
-        super(EfficientNet, self).__init__(gls)
+    def __init__(self, gls_create: function):
+        super(EfficientNet, self).__init__(gls_create)
         self.efficientnet = models.efficientnet_b7(pretrained=True)
         self.base = nn.Sequential(*list(self.efficientnet.children())[:-1]) # remove last layer
+        self.gls = self.gls_crate(2560)
